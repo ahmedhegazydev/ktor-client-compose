62a51091c971c35a006db3075c4cc529ec5edfa4
ahmedhegazy <ahmed.hegazy@safavisa.com>
2021-10-04 16:13:19 +0200

    List all posts

--- app/src/main/java/com/hegazy/ktorclientcompose/MainActivity.kt
+++ app/src/main/java/com/hegazy/ktorclientcompose/MainActivity.kt
@@ -3,21 +3,57 @@ package com.hegazy.ktorclientcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.produceState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.hegazy.ktorclientcompose.data.remote.PostsService
+import com.hegazy.ktorclientcompose.data.remote.PostsServiceImpl
+import com.hegazy.ktorclientcompose.data.remote.dto.PostResponse
 import com.hegazy.ktorclientcompose.ui.theme.KtorClientComposeTheme
 
 class MainActivity : ComponentActivity() {
+
+
+    private val postsService = PostsService.create()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+            val posts = produceState<List<PostResponse>>(
+                initialValue = emptyList(),
+                producer = {
+                    value = postsService.getPosts()
+
+                })
+
             KtorClientComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+//                    Greeting("Android")
+                    LazyColumn {
+                        items(posts.value) {
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(5.dp)
+                            ) {
+                                Text(text = it.title, fontSize = 20.sp)
+                                Spacer(modifier = Modifier.height(4.dp))
+                                Text(text = it.body, fontSize = 16.sp)
+                            }
+                        }
+                    }
                 }
             }
         }
--- app/src/main/java/com/hegazy/ktorclientcompose/data/remote/PostsServive.kt
+++ app/src/main/java/com/hegazy/ktorclientcompose/data/remote/PostsServive.kt
@@ -4,6 +4,9 @@ import com.hegazy.ktorclientcompose.data.remote.dto.PostRequest
 import com.hegazy.ktorclientcompose.data.remote.dto.PostResponse
 import io.ktor.client.*
 import io.ktor.client.engine.android.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.features.logging.*
 
 interface PostsService {
 
@@ -17,6 +20,12 @@ interface PostsService {
         fun create(): PostsService{
             return PostsServiceImpl(
                 client = HttpClient(Android){
+                    install(Logging){
+                        level = LogLevel.ALL
+                    }
+                    install(JsonFeature){
+                        serializer = KotlinxSerializer()
+                    }
 
                 }
             )
